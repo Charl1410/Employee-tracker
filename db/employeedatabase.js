c8c2d1e958bf8b4f3078b3e6b3bd88dc223d7e8a
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2023-02-07 14:02:18 +0000

    second insert st for getting roles with department name added

--- db/employeedatabase.js
+++ db/employeedatabase.js
@@ -7,6 +7,7 @@ class EmployeeDatabase extends Database {
 
     getDepartments() {
 
+        //selecting all the data from the department database and will be passed into the view departments function in index.js
         return new Promise((resolve, reject) => {
             this.db.query('SELECT * FROM department'), (err, results) => {
                 if (err) {
@@ -20,7 +21,7 @@ class EmployeeDatabase extends Database {
     getRoles() {
 
         return new Promise((resolve, reject) => {
-            this.db.query('SELECT'), (err, results) => {
+            this.db.query('SELECT role.id role.title role.salary department.name AS department_name FROM department INNER JOIN role ON role.deparment_id = department.id'), (err, results) => {
                 if (err) {
                     reject(err);
                 }
